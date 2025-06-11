34485496aca4ab522c3b351a0331034973437457
Arthur e Henry <nunesarthur768@gmail.com>
2025-06-11 10:34:22 -0300

    Criando label login

--- ExemploAula/Form1.Designer.cs
+++ ExemploAula/Form1.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            lbllogin = new Label();
+            SuspendLayout();
+            // 
+            // lbllogin
+            // 
+            lbllogin.AutoSize = true;
+            lbllogin.Font = new Font("Arial Narrow", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbllogin.Location = new Point(325, 54);
+            lbllogin.Name = "lbllogin";
+            lbllogin.Size = new Size(59, 23);
+            lbllogin.TabIndex = 0;
+            lbllogin.Text = "LOGIN";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lbllogin);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lbllogin;
     }
 }
